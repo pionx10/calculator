33623be3ca71548a58fe0e325e238bfe5ff99f6b
PRahamim <prahamim@ilionx.com>
2022-08-11 14:23:41 +0200

    functionality works but tests fail

--- src/main/java/com/example/springbootcalculator/CalculatorService.java
+++ src/main/java/com/example/springbootcalculator/CalculatorService.java
@@ -1,30 +1,60 @@
 package com.example.springbootcalculator;
 
+import com.example.springbootcalculator.entity.Args;
+import com.example.springbootcalculator.entity.Calculation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    @Autowired
+    private CalculatorRepository calculatorRepository;
+
+    public Iterable<Calculation> findAll() {
+        return calculatorRepository.findAll();
+    }
+
+    public boolean save(String str) {
+        try {
+            Calculation cal = new Calculation();
+            cal.setOperation(str);
+            this.calculatorRepository.save(cal);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     public double add(Args args) {
         int a = args.getA();
         int b = args.getB();
-        return (double)a+b;
+        Double ans = (double)a+b;
+        save(a + " + " + b + " = " + ans);
+        return ans;
     }
 
     public double subtract(Args args) {
         int a = args.getA();
         int b = args.getB();
-        return (double)a-b;
+        Double ans = (double)a-b;
+        save(a + " - " + b + " = " + ans);
+        return ans;
     }
 
     public double multiply(Args args) {
         int a = args.getA();
         int b = args.getB();
-        return (double)a*b;
+        Double ans = (double)a*b;
+        save(a + " * " + b + " = " + ans);
+        return ans;
     }
 
     public double divide(Args args) {
         int a = args.getA();
         int b = args.getB();
-        return (double)a/b;
+        Double ans = (double)a/b;
+        save(a + " / " + b + " = " + ans);
+        return ans;
     }
 }
