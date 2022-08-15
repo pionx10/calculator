342a9630eab707efc4bbd1aab3389db760fbed55
PRahamim <prahamim@ilionx.com>
2022-08-15 12:18:38 +0200

    frontend calculator operations

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,12 +1,29 @@
 function calculate(){
+    let a = parseInt(document.getElementById("a").value,10);
+    let b = parseInt(document.getElementById("b").value,10);
+    let action = document.getElementById("action").value;
 
+    let link = "/api/" + action;
+    let xhReq = new XMLHttpRequest();
+
+    xhReq.open("POST", link, false);
+
+    let data = {
+        a: a,
+        b: b
+    };
+
+    xhReq.setRequestHeader("Content-Type", "application/json");
+    xhReq.send(JSON.stringify(data));
+
+    document.getElementById('answer').innerHTML = JSON.parse(xhReq.responseText);
 }
 
 function showHistory(){
-    var link = "/api/history";
-    var xhReq = new XMLHttpRequest();
+    let link = "/api/history";
+    let xhReq = new XMLHttpRequest();
     xhReq.open("GET", link, false);
     xhReq.send(null);
-    var jsonObject = JSON.parse(xhReq.responseText);
+    let jsonObject = JSON.parse(xhReq.responseText);
     document.getElementById('historyTable').innerHTML = JSON.stringify(jsonObject);
 }
